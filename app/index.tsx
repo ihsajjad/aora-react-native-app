5dd4277a9d4a8c722128605fd0c6f6b343662d95
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-06-12 11:28:25 +0600

    added nedded fonts and icons

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <View>
+      <Text className="text-3xl font-semibold mt-20">App</Text>
+    </View>
+  );
+};
+
+export default App;
